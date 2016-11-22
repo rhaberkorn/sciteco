rhaberkorn/sciteco

commit cb5e08b40d7444817c7eb6c1e4e8bf5208c2823c

improved command line option handling
 * it turns out that option-like arguments could not be reliably passed to
   SciTECO scripts for two reasons:
   a) "--" arguments are not removed from argv by GOption if it detects
      and following option-like argument.
      "--" would thus be passed as a script argument which will disable
      option parsing in scripts that interpret "--".
   b) A script run via the Hash-Bang line "#!...sciteco -m" would
      require an explicit "--" to turn of GOption parsing.
      However it is __impossible__ to insert after the script file name
      on UNIX.
 * Therefore, SciTECO now removes leading "--" arguments left over by GOption.
 * If possible (Glib >= 2.44), option parsing is performed in strict POSIX
   mode which inhibits parsing after the first non-option argument.
   This reduces the number of cases where an explicit "--" is required.
 * --mung no longer takes an argument. Instead, the first non-option argument
   is expected to be the script file name.
   This looks weird at first but is more consistent with how other interpeters
   work. Once we revise argument passing to scripts, the script name can also
   be passed to the script which is more consistent with it being the first
   non-option argument.
   Also, with strict POSIX parsing, this fixed Hash-Bang lines since
   the script file name constructed by the kernel will automatically switch
   off option parsing, passing all option-like script arguments uninterpreted
   to the script.
 * Since we're supporting Glib < 2.44, the Hash-Bang lines are still broken
   for certain builds.
   Therefore, a wrapper script is installed to libexecdir (it never has to be
   executed by users and Hash-Bang lines need absolute paths anyway) which
   transparently inserts "--" into the SciTECO command line and should be used
   as the interpreter in portable SciTECO scripts.
   The wrapper script is generated and points to the exact SciTECO binary

src/interface-gtk/interface-gtk.cpp
@@ -183,7 +183,12 @@ InterfaceGtk::get_options(void)
 		{NULL}
 	};
 
-	GOptionGroup *group = gtk_get_option_group(TRUE);
+	/*
+	 * Parsing the option context with the Gtk option group
+	 * will automatically initialize Gtk, so gtk_init()
+	 * does not have to be called.
+	 */
+	GOptionGroup *group = gtk_get_option_group(FALSE);
 
 	g_option_group_add_entries(group, entries);
 
@@ -191,7 +196,7 @@ InterfaceGtk::get_options(void)
 }
 
 void
-InterfaceGtk::main_impl(int &argc, char **&argv)
+InterfaceGtk::init(void)
 {
 	static const Cmdline empty_cmdline;
 
@@ -208,7 +213,6 @@ InterfaceGtk::main_impl(int &argc, char **&argv)
 	g_thread_init(NULL);
 #endif
 	gdk_threads_init();
-	gtk_init(&argc, &argv);
 
 	/*
 	 * Register clipboard registers.

.gitignore
@@ -31,6 +31,7 @@ testsuite.dir
 # Binaries
 /src/sciteco
 /src/sciteco-minimal
+/src/sciteco-wrapper
 
 # Generated source files
 /src/interface-gtk/gtk-info-popup.[ch]

bootstrap.am
@@ -25,6 +25,7 @@ SUBST_MACRO = eb$<\e \
 	      <fs@PACKAGE_URL^Q@\e@PACKAGE_URL@\e;>j \
 	      <fs@PACKAGE_URL_DEV^Q@\e@PACKAGE_URL_DEV@\e;>j \
 	      <fs@bindir^Q@\e$(bindir)\e;>j \
+	      <fs@libexecdir^Q@\e$(libexecdir)\e;>j \
 	      <fs@pkgdatadir^Q@\e$(pkgdatadir)\e;>j \
 	      <fs@scitecolibdir^Q@\e$(scitecolibdir)\e;>j \
 	      <fs@TECO_INTEGER^Q@\e@TECO_INTEGER@\e;>j \

doc/grosciteco.tes
@@ -1,4 +1,4 @@
-#!/usr/bin/env sciteco -m
+#!/usr/local/bin/sciteco -m
 !* grosciteco.tes <output-woman> <output-tec> <input> *!
 
 !* Process command-line options *!

doc/htbl.tes
@@ -1,4 +1,4 @@
-#!/usr/bin/env sciteco -m
+#!/usr/local/bin/sciteco -m
 !* htbl.tes <input> <output> *!
 !* Troff tbl "drop-in" replacement *!
 

doc/sciteco.1.in
@@ -17,10 +17,11 @@ Scintilla-based \fBT\fPext \fBE\fPditor and \fBCO\fPrrector
 .SY @PACKAGE@
 .OP "-h|--help"
 .OP "-e|--eval" macro
-.OP "-m|--mung" file
+.OP "-m|--mung"
 .OP "--no-profile"
 .RI [ "UI option .\|.\|." ]
 .OP "--"
+.RI [ script ]
 .RI [ "argument .\|.\|." ]
 .YS
 .
@@ -40,26 +41,46 @@ natively supports Microsoft Windows NT\*(Tm.
 .LP
 .SCITECO_TOPIC mung
 When executed, \*(ST mungs (executes) the TECO macro stored in the file
-specified by the
+specified in the \fIscript\fP argument if
 .B "--mung"
-option or the macro specified via
+is given or the macro specified via
 .B "--eval"
 respectively.
 Munged files and macros are executed in non-interactive (\fIbatch\fP)
 mode, allowing the user to write stand-alone TECO scripts.
 Only when munging files as opposed to other means of executing macros,
-the first line is ignored if it begins with \(lq#!\(rq.
+the first line is ignored if it begins with a \(lq#\(rq (hash sign).
 Therefore under UNIX-like operating systems, TECO macro files may be
 invoked as scripts by using a Hash-Bang line like
 .\" FIXME: We'd like to include #! as a topic, but ! character are currently
 .\" not allowed since they are not escaped correctly.
 .SCITECO_TOPIC scripting
 .RS
-.SCITECO_TT
 .EX
+.SCITECO_TT
 #!@bindir@/sciteco -m
-.EE
 .SCITECO_TT_END
+.EE
+.RE
+.
+.LP
+Note that UNIX Hash-Bang lines will only pass a \fBsingle\fP argument to the
+interpreter before the script's file name, so all required \*(ST options must
+be mangled into a single argument with their single-letter names.
+Passing option-like arguments (beginning with a dash) to scripts may cause
+problems because \*(ST might try to interpret these options.
+Beginning with Glib 2.44, \*(ST thus stops parsing at the first non-option
+argument (which will always be the munged file name in a script invocation).
+For binaries linked against older versions of Glib, \*(ST works around this
+issue by providing a wrapper script that can be used in place of the main
+executable.
+A portable Hash-Bang line should thus look like:
+.RS
+.EX
+.SCITECO_TT
+#!@libexecdir@/sciteco-wrapper -m
+.SCITECO_TT_END
+.EE
 .RE
 .
 .LP
@@ -69,6 +90,8 @@ All command line arguments after the \*(ST options are passed as
 .I arguments
 to the munged macro by placing each argument on its own line in
 the buffer.
+The \fIscript\fP file name expected when \(lq--mung\(rq is given
+is currently \fBnot\fP considered a macro argument.
 In any case the current buffer position (called
 .IR dot )
 is left at the beginning of the buffer.
@@ -137,6 +160,8 @@ option is absent, \*(ST will mung
 On UNIX/Linux, the default profile is at
 .I ~/.teco_ini
 (see \fBENVIRONMENT\fP).
+It will consequently not expect a \fIscript\fP file name as
+the first non-option argument.
 The profile will usually set up various Scintilla and \*(ST options,
 configure syntax highlighting,
 define commonly used macros and open files specified as arguments to \*(ST.
@@ -160,9 +185,12 @@ Similar to munging but always exits afterwards.
 If the option is specified, the
 .B \-\-mung
 option has no effect.
-.IP "\fB-m\fR, \fB--mung\fR \fIfile"
+.IP "\fB-m\fR, \fB--mung\fR"
 .SCITECO_TOPIC "-m" "--mung"
-Mung \fIfile\fP.
+Mung \fIscript\fP.
+The script file name is expected as the first non-option
+argument, so it does not necessarily have to follow the
+\fB--mung\fP option.
 Default is
 .IR $SCITECOCONFIG/.teco_ini .
 .IP "\fB--no-profile\fP"

src/Makefile.am
@@ -76,10 +76,27 @@ CLEANFILES = $(BUILT_SOURCES) \
 
 symbols-scintilla.cpp : @SCINTILLA_PATH@/include/Scintilla.h \
 			symbols-extract.tes
-	$(SCITECO_MINIMAL) -m @srcdir@/symbols-extract.tes \
+	$(SCITECO_MINIMAL) -m -- @srcdir@/symbols-extract.tes \
 	                   $< $@ "SCI_" scintilla
 
 symbols-scilexer.cpp : @SCINTILLA_PATH@/include/SciLexer.h \
 		       symbols-extract.tes
-	$(SCITECO_MINIMAL) -m @srcdir@/symbols-extract.tes \
+	$(SCITECO_MINIMAL) -m -- @srcdir@/symbols-extract.tes \
 	                   $< $@ "SCLEX_,SCE_" scilexer
+
+# This installs a wrapper script to libexecdir to be used as
+# the SciTECO interpreter in Hash-Bang lines.
+# It makes sure that option parsing is disabled for all
+# script arguments which is necessary for builds against Glib < 2.44.
+# NOTE: When we raise the Glib requirement to 2.44, the sciteco-wrapper
+# workaround can be removed completely.
+libexec_SCRIPTS = sciteco-wrapper
+CLEANFILES += $(libexec_SCRIPTS)
+
+.PHONY: sciteco-wrapper
+sciteco-wrapper:
+	echo '#!/bin/sh' >$@
+	echo 'OPT=$$1;' \
+	     'shift;' \
+	     "exec $(bindir)/`echo sciteco | @SED@ '$(transform)'`$(EXEEXT)" \
+	     '"$$OPT" -- $$@' >>$@

src/interface-curses/interface-curses.cpp
@@ -312,7 +312,7 @@ InterfaceCurses::InterfaceCurses() : stdout_orig(-1), stderr_orig(-1),
 }
 
 void
-InterfaceCurses::main_impl(int &argc, char **&argv)
+InterfaceCurses::init(void)
 {
 	/*
 	 * We must register this handler to handle
@@ -518,12 +518,16 @@ InterfaceCurses::init_screen(void)
 	 * This sets the program name to "SciTECO"
 	 * which may then also be used as the X11 class name
 	 * for overwriting X11 resources in .Xdefaults
+	 *
 	 * FIXME: We could support passing in resource
 	 * overrides via the SciTECO command line.
 	 * But unfortunately, Xinitscr() is called too
-	 * late to modify argc/argv for command-line parsing.
+	 * late to modify argc/argv for command-line parsing
+	 * (and GOption needs to know about the additional
+	 * possible arguments since they are not passed through
+	 * transparently).
 	 * Therefore this could only be supported by
-	 * adding a special option like --resource.
+	 * adding a special option like --resource KEY=VAL.
 	 */
 	Xinitscr(1, (char **)argv);
 }

commit 09cd95bb27b18a83a3028399236ee5309327bad8

Gtk interface: make sure that the default display is opened
 * this has been broken since cb5e08b40d

src/interface-gtk/interface-gtk.cpp
@@ -185,8 +185,8 @@ InterfaceGtk::get_options(void)
 
 	/*
 	 * Parsing the option context with the Gtk option group
-	 * will automatically initialize Gtk, so gtk_init()
-	 * does not have to be called.
+	 * will automatically initialize Gtk, but we do not yet
+	 * open the default display.
 	 */
 	GOptionGroup *group = gtk_get_option_group(FALSE);
 
@@ -213,6 +213,14 @@ InterfaceGtk::init(void)
 	g_thread_init(NULL);
 #endif
 	gdk_threads_init();
+
+	/*
+	 * gtk_init() is not necessary when using gtk_get_option_group(),
+	 * but this will open the default display.
+	 * FIXME: Perhaps it is possible to defer this until we initialize
+	 * interactive mode!?
+	 */
+	gtk_init(NULL, NULL);
 
 	/*
 	 * Register clipboard registers.