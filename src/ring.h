70619bb88918e9cd057dbbc6a87e890cbce49a08
Robin Haberkorn <robin.haberkorn@googlemail.com>
2022-06-21 03:41:16 +0200

    updated copyright to 2022 and updated TODO

--- src/ring.h
+++ src/ring.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2021 Robin Haberkorn
+ * Copyright (C) 2012-2022 Robin Haberkorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
