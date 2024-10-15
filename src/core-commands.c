rhaberkorn/sciteco

commit b31b88717172e22b49c0493185f603b8f84989ec

the ^EUq string building escape now respects the encoding (can insert bytes or codepoints) (refs #5)
* This is trickier than it sounds because there isn't one single place to consult.
  It depends on the context.
  If the string argument relates to buffer contents - as in <I>, <S>, <FR> etc. -
  the buffer's encoding is consulted.
  If it goes into a register (EU), the register's encoding is consulted.
  Everything else (O, EN, EC, ES...) expects only Unicode codepoints.
* This is communicated through a new field teco_machine_stringbuilding_t::codepage
  which must be set in the states' initial callback.
* Seems overkill just for ^EUq, but it can be used for context-sensitive
  processing of all the other string building constructs as well.
* ^V and ^W cannot be supported for Unicode characters for the time being without an Unicode-aware parser

src/core-commands.c
@@ -2494,8 +2494,7 @@ teco_state_ecommand_encoding(teco_machine_main_t *ctx, GError **error)
 
 	gboolean colon_modified = teco_machine_main_eval_colon(ctx);
 
-	sptr_t old_cp = teco_interface_ssm(SCI_GETCODEPAGE, 0, 0)
-				? : teco_interface_ssm(SCI_STYLEGETCHARACTERSET, STYLE_DEFAULT, 0);
+	guint old_cp = teco_interface_get_codepage();
 
 	if (!teco_expressions_args()) {
 		/* get current code page */
@@ -2744,6 +2743,12 @@ teco_state_insert_initial(teco_machine_main_t *ctx, GError **error)
 {
 	if (ctx->mode > TECO_MODE_NORMAL)
 		return TRUE;
+
+	/*
+	 * Current document's encoding determines the behaviour of
+	 * string building constructs.
+	 */
+	teco_undo_guint(ctx->expectstring.machine.codepage) = teco_interface_get_codepage();
 
 	if (!teco_expressions_eval(FALSE, error))
 		return FALSE;

src/parser.c
@@ -410,6 +410,7 @@ teco_state_stringbuilding_escaped_input(teco_machine_stringbuilding_t *ctx, gcha
 		/* parse-only mode */
 		return &teco_state_stringbuilding_start;
 
+	/* FIXME: Consult ctx->codepage once we have an Unicode-conforming parser */
 	switch (ctx->mode) {
 	case TECO_STRINGBUILDING_MODE_UPPER:
 		chr = g_ascii_toupper(chr);
@@ -442,6 +443,7 @@ teco_state_stringbuilding_lower_input(teco_machine_stringbuilding_t *ctx, gchar 
 			teco_undo_guint(ctx->mode);
 		ctx->mode = TECO_STRINGBUILDING_MODE_LOWER;
 	} else {
+		/* FIXME: Consult ctx->codepage once we have an Unicode-conforming parser */
 		teco_string_append_c(ctx->result, g_ascii_tolower(chr));
 	}
 
@@ -465,6 +467,7 @@ teco_state_stringbuilding_upper_input(teco_machine_stringbuilding_t *ctx, gchar 
 			teco_undo_guint(ctx->mode);
 		ctx->mode = TECO_STRINGBUILDING_MODE_UPPER;
 	} else {
+		/* FIXME: Consult ctx->codepage once we have an Unicode-conforming parser */
 		teco_string_append_c(ctx->result, g_ascii_toupper(chr));
 	}
 
@@ -576,15 +579,28 @@ teco_state_stringbuilding_ctle_u_input(teco_machine_stringbuilding_t *ctx, gchar
 	teco_int_t value;
 	if (!qreg->vtable->get_integer(qreg, &value, error))
 		return NULL;
-	if (value < 0 || value > 0xFF) {
-		g_autofree gchar *name_printable = teco_string_echo(qreg->head.name.data, qreg->head.name.len);
-		g_set_error(error, TECO_ERROR, TECO_ERROR_FAILED,
-		            "Q-Register \"%s\" does not contain a valid character", name_printable);
-		return NULL;
+
+	if (ctx->codepage == SC_CP_UTF8) {
+		if (value < 0 || !g_unichar_validate(value))
+			goto error_codepoint;
+		/* 4 bytes should be enough, but we better follow the documentation */
+		gchar buf[6];
+		gsize len = g_unichar_to_utf8(value, buf);
+		teco_string_append(ctx->result, buf, len);
+	} else {
+		if (value < 0 || value > 0xFF)
+			goto error_codepoint;
+		teco_string_append_c(ctx->result, (gchar)value);
 	}
 
-	teco_string_append_c(ctx->result, (gchar)value);
 	return &teco_state_stringbuilding_start;
+
+error_codepoint: {
+	g_autofree gchar *name_printable = teco_string_echo(qreg->head.name.data, qreg->head.name.len);
+	g_set_error(error, TECO_ERROR, TECO_ERROR_CODEPOINT,
+	            "Q-Register \"%s\" does not contain a valid codepoint", name_printable);
+	return NULL;
+}
 }
 
 TECO_DEFINE_STATE_STRINGBUILDING_QREG(teco_state_stringbuilding_ctle_u);
@@ -708,6 +724,7 @@ teco_machine_stringbuilding_init(teco_machine_stringbuilding_t *ctx, gchar escap
 	teco_machine_init(&ctx->parent, &teco_state_stringbuilding_start, must_undo);
 	ctx->escape_char = escape_char;
 	ctx->qreg_table_locals = locals;
+	ctx->codepage = SC_CP_UTF8;
 }
 
 void
@@ -744,6 +761,14 @@ teco_machine_stringbuilding_clear(teco_machine_stringbuilding_t *ctx)
 {
 	if (ctx->machine_qregspec)
 		teco_machine_qregspec_free(ctx->machine_qregspec);
+}
+
+gboolean
+teco_state_expectstring_initial(teco_machine_main_t *ctx, GError **error)
+{
+	if (ctx->mode == TECO_MODE_NORMAL)
+		teco_undo_guint(ctx->expectstring.machine.codepage) = SC_CP_UTF8;
+	return TRUE;
 }
 
 teco_state_t *

src/qreg-commands.c
@@ -470,6 +470,23 @@ TECO_DEFINE_STATE_EXPECTQREG(teco_state_eucommand,
 	.expectqreg.type = TECO_QREG_OPTIONAL_INIT
 );
 
+static gboolean
+teco_state_setqregstring_building_initial(teco_machine_main_t *ctx, GError **error)
+{
+	if (ctx->mode > TECO_MODE_NORMAL)
+		return TRUE;
+
+	teco_qreg_t *qreg;
+	teco_machine_qregspec_get_results(ctx->expectqreg, &qreg, NULL);
+
+	/*
+	 * The expected codepage of string building constructs is determined
+	 * by the Q-Register.
+	 */
+	teco_undo_guint(ctx->expectstring.machine.codepage) = qreg->vtable->get_codepage(qreg);
+	return TRUE;
+}
+
 static teco_state_t *
 teco_state_setqregstring_building_done(teco_machine_main_t *ctx, const teco_string_t *str, GError **error)
 {
@@ -487,6 +504,7 @@ teco_state_setqregstring_building_done(teco_machine_main_t *ctx, const teco_stri
  * characters \fBenabled\fP.
  */
 TECO_DEFINE_STATE_EXPECTSTRING(teco_state_setqregstring_building,
+	.initial_cb = (teco_state_initial_cb_t)teco_state_setqregstring_building_initial,
 	.expectstring.string_building = TRUE
 );
 

src/search.c
@@ -60,6 +60,8 @@ teco_state_search_initial(teco_machine_main_t *ctx, GError **error)
 	if (ctx->mode > TECO_MODE_NORMAL)
 		return TRUE;
 
+	teco_undo_guint(ctx->expectstring.machine.codepage) = teco_interface_get_codepage();
+
 	if (G_UNLIKELY(!teco_search_qreg_machine))
 		teco_search_qreg_machine = teco_machine_qregspec_new(TECO_QREG_REQUIRED, ctx->qreg_table_locals,
 		                                                     ctx->parent.must_undo);
@@ -978,11 +980,19 @@ teco_state_search_delete_done(teco_machine_main_t *ctx, const teco_string_t *str
  */
 TECO_DEFINE_STATE_SEARCH(teco_state_search_delete);
 
+static gboolean
+teco_state_replace_insert_initial(teco_machine_main_t *ctx, GError **error)
+{
+	if (ctx->mode == TECO_MODE_NORMAL)
+		teco_undo_guint(ctx->expectstring.machine.codepage) = teco_interface_get_codepage();
+	return TRUE;
+}
+
 /*
  * FIXME: Could be static
  */
 TECO_DEFINE_STATE_INSERT(teco_state_replace_insert,
-	.initial_cb = NULL
+	.initial_cb = (teco_state_initial_cb_t)teco_state_replace_insert_initial
 );
 
 static teco_state_t *

src/spawn.c
@@ -164,6 +164,11 @@ teco_state_execute_initial(teco_machine_main_t *ctx, GError **error)
 	if (ctx->mode > TECO_MODE_NORMAL)
 		return TRUE;
 
+	/*
+	 * Command-lines and file names are always assumed to be UTF-8.
+	 */
+	teco_undo_guint(ctx->expectstring.machine.codepage) = SC_CP_UTF8;
+
 	if (!teco_expressions_eval(FALSE, error))
 		return FALSE;
 

doc/sciteco.7.template
@@ -1647,6 +1647,12 @@ Expands to the character whose code is stored in the numeric
 part of Q-Register \fIq\fP.
 For instance if register \(lqA\(rq contains the code 66,
 \(lq^EUa\(rq expands to the character \(lqB\(rq.
+The interpretation of this code depends on the context.
+Within inserts and searches (\fBI\fP, \fBS\fP, etc.) bytes or Unicode codepoints
+are expected depending on the buffer's encoding.
+Operations on registers (\fBEU\fP) similarily consult the
+register's encoding.
+Everything else expects Unicode codepoints.
 .TP
 .SCITECO_TOPIC ^EQ ^EQq
 .BI ^EQ q

src/interface.h
@@ -154,6 +154,12 @@ void teco_interface_process_notify(SCNotification *notify);
 /** @pure */
 void teco_interface_cleanup(void);
 
+static inline guint
+teco_interface_get_codepage(void)
+{
+	return teco_view_get_codepage(teco_interface_current_view);
+}
+
 static inline gssize
 teco_glyphs2bytes(teco_int_t pos)
 {

src/parser.h
@@ -309,9 +309,6 @@ typedef enum {
 /**
  * A stringbuilding state machine.
  *
- * @fixme Should contain the escape char (currently in teco_machine_expectstring_t),
- * so that we can escape it via ^Q.
- *
  * @extends teco_machine_t
  */
 typedef struct teco_machine_stringbuilding_t {
@@ -350,6 +347,13 @@ typedef struct teco_machine_stringbuilding_t {
 	 * (see teco_state_stringbuilding_start_process_edit_cmd()).
 	 */
 	teco_string_t *result;
+
+	/**
+	 * Encoding of string in `result`.
+	 * This is inherited from the embedding command and may depend on
+	 * the buffer's or Q-Register's encoding.
+	 */
+	guint codepage;
 } teco_machine_stringbuilding_t;
 
 void teco_machine_stringbuilding_init(teco_machine_stringbuilding_t *ctx, gchar escape_char,
@@ -508,6 +512,7 @@ void teco_machine_main_clear(teco_machine_main_t *ctx);
 
 G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC(teco_machine_main_t, teco_machine_main_clear);
 
+gboolean teco_state_expectstring_initial(teco_machine_main_t *ctx, GError **error);
 teco_state_t *teco_state_expectstring_input(teco_machine_main_t *ctx, gchar chr, GError **error);
 gboolean teco_state_expectstring_refresh(teco_machine_main_t *ctx, GError **error);
 
@@ -533,6 +538,7 @@ gboolean teco_state_expectstring_process_edit_cmd(teco_machine_main_t *ctx, teco
 		return teco_state_expectstring_input(ctx, chr, error); \
 	} \
 	TECO_DEFINE_STATE(NAME, \
+		.initial_cb = (teco_state_initial_cb_t)teco_state_expectstring_initial, \
 		.refresh_cb = (teco_state_refresh_cb_t)teco_state_expectstring_refresh, \
 		.process_edit_cmd_cb = (teco_state_process_edit_cmd_cb_t) \
 		                       teco_state_expectstring_process_edit_cmd, \

commit d556aee67e615b48c25861741d28d103109235e3

fixed memory corruptions due to undoing the teco_machine_stringbuilding_t::codepage
* It's contained in teco_machine_main_t which is created per macro call frame.
  So after macro calls, the machine no longer exists.
  It is therefore unsafe to undo its members indiscriminately.
* On the other hand, we must undo the codepage setting when run interactively,
  so it is now only undone when belonging to the commandline macro frame.
* This was actually causing memory corruptions on every fnkeys cursor movement, but never
  caused crashes - probably because the invalid pointers are always pointing to unused
  parts of the C call stack.
* Initially broken in b31b8871.

src/core-commands.c
@@ -2759,8 +2759,12 @@ teco_state_insert_initial(teco_machine_main_t *ctx, GError **error)
 	/*
 	 * Current document's encoding determines the behaviour of
 	 * string building constructs.
+	 *
+	 * NOTE: This is not safe to undo in macro calls.
 	 */
-	teco_undo_guint(ctx->expectstring.machine.codepage) = teco_interface_get_codepage();
+	if (ctx == &teco_cmdline.machine)
+		teco_undo_guint(ctx->expectstring.machine.codepage);
+	ctx->expectstring.machine.codepage = teco_interface_get_codepage();
 
 	if (!teco_expressions_eval(FALSE, error))
 		return FALSE;

src/parser.c
@@ -35,6 +35,7 @@
 #include "ring.h"
 #include "glob.h"
 #include "error.h"
+#include "cmdline.h"
 #include "core-commands.h"
 #include "goto-commands.h"
 #include "parser.h"
@@ -906,8 +907,15 @@ teco_machine_stringbuilding_clear(teco_machine_stringbuilding_t *ctx)
 gboolean
 teco_state_expectstring_initial(teco_machine_main_t *ctx, GError **error)
 {
-	if (ctx->mode == TECO_MODE_NORMAL)
-		teco_undo_guint(ctx->expectstring.machine.codepage) = teco_default_codepage();
+	if (ctx->mode > TECO_MODE_NORMAL)
+		return TRUE;
+
+	/*
+	 * NOTE: This is not safe to undo in macro calls.
+	 */
+	if (ctx == &teco_cmdline.machine)
+		teco_undo_guint(ctx->expectstring.machine.codepage);
+	ctx->expectstring.machine.codepage = teco_default_codepage();
 	return TRUE;
 }
 

src/qreg-commands.c
@@ -29,6 +29,7 @@
 #include "interface.h"
 #include "ring.h"
 #include "parser.h"
+#include "cmdline.h"
 #include "core-commands.h"
 #include "qreg.h"
 #include "qreg-commands.h"
@@ -492,8 +493,11 @@ teco_state_setqregstring_building_initial(teco_machine_main_t *ctx, GError **err
 	/*
 	 * The expected codepage of string building constructs is determined
 	 * by the Q-Register.
+	 *
+	 * NOTE: This is not safe to undo in macro calls.
 	 */
-	teco_undo_guint(ctx->expectstring.machine.codepage);
+	if (ctx == &teco_cmdline.machine)
+		teco_undo_guint(ctx->expectstring.machine.codepage);
 	return qreg->vtable->get_string(qreg, NULL, NULL, &ctx->expectstring.machine.codepage, error);
 }
 

src/search.c
@@ -35,6 +35,7 @@
 #include "parser.h"
 #include "core-commands.h"
 #include "error.h"
+#include "cmdline.h"
 #include "search.h"
 
 typedef struct {
@@ -60,7 +61,12 @@ teco_state_search_initial(teco_machine_main_t *ctx, GError **error)
 	if (ctx->mode > TECO_MODE_NORMAL)
 		return TRUE;
 
-	teco_undo_guint(ctx->expectstring.machine.codepage) = teco_interface_get_codepage();
+	/*
+	 * NOTE: This is not safe to undo in macro calls.
+	 */
+	if (ctx == &teco_cmdline.machine)
+		teco_undo_guint(ctx->expectstring.machine.codepage);
+	ctx->expectstring.machine.codepage = teco_interface_get_codepage();
 
 	if (G_UNLIKELY(!teco_search_qreg_machine))
 		teco_search_qreg_machine = teco_machine_qregspec_new(TECO_QREG_REQUIRED, ctx->qreg_table_locals,
@@ -1046,8 +1052,15 @@ TECO_DEFINE_STATE_SEARCH(teco_state_search_delete);
 static gboolean
 teco_state_replace_insert_initial(teco_machine_main_t *ctx, GError **error)
 {
-	if (ctx->mode == TECO_MODE_NORMAL)
-		teco_undo_guint(ctx->expectstring.machine.codepage) = teco_interface_get_codepage();
+	if (ctx->mode > TECO_MODE_NORMAL)
+		return TRUE;
+
+	/*
+	 * NOTE: This is not safe to undo in macro calls.
+	 */
+	if (ctx == &teco_cmdline.machine)
+		teco_undo_guint(ctx->expectstring.machine.codepage);
+	ctx->expectstring.machine.codepage = teco_interface_get_codepage();
 	return TRUE;
 }
 

src/spawn.c
@@ -43,6 +43,7 @@
 #include "core-commands.h"
 #include "qreg-commands.h"
 #include "error.h"
+#include "cmdline.h"
 #include "spawn.h"
 
 /*
@@ -167,8 +168,12 @@ teco_state_execute_initial(teco_machine_main_t *ctx, GError **error)
 	/*
 	 * Command-lines and file names are always assumed to be UTF-8,
 	 * unless we set TECO_ED_DEFAULT_ANSI.
+	 *
+	 * NOTE: This is not safe to undo in macro calls.
 	 */
-	teco_undo_guint(ctx->expectstring.machine.codepage) = teco_default_codepage();
+	if (ctx == &teco_cmdline.machine)
+		teco_undo_guint(ctx->expectstring.machine.codepage);
+	ctx->expectstring.machine.codepage = teco_default_codepage();
 
 	if (!teco_expressions_eval(FALSE, error))
 		return FALSE;