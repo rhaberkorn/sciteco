f4fd6de46a8d66032efd6fa86a5674edd0ec839f
Robin Haberkorn <robin.haberkorn@googlemail.com>
2024-12-22 18:12:21 +0300

    fixed indention in interface-curses/interface.c
    This is a purely cosmetic change.

--- src/interface-curses/interface.c
+++ src/interface-curses/interface.c
@@ -1706,15 +1706,15 @@ teco_interface_event_loop_iter(void)
 			return;
 
 #ifdef __PDCURSES__
-	/*
-	 * Especially PDCurses/WinGUI likes to report two keypresses,
-	 * e.g. for CTRL+Shift+6 (CTRL+^).
-	 * Make sure we don't filter out AltGr, which may be reported as CTRL+ALT.
-	 */
-	if ((PDC_get_key_modifiers() &
-	     (PDC_KEY_MODIFIER_CONTROL | PDC_KEY_MODIFIER_ALT)) == PDC_KEY_MODIFIER_CONTROL &&
-	    !TECO_IS_CTL(key))
-		return;
+		/*
+		 * Especially PDCurses/WinGUI likes to report two keypresses,
+		 * e.g. for CTRL+Shift+6 (CTRL+^).
+		 * Make sure we don't filter out AltGr, which may be reported as CTRL+ALT.
+		 */
+		if ((PDC_get_key_modifiers() &
+		     (PDC_KEY_MODIFIER_CONTROL | PDC_KEY_MODIFIER_ALT)) == PDC_KEY_MODIFIER_CONTROL &&
+		    !TECO_IS_CTL(key))
+			return;
 #endif
 
 		/*
