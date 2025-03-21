rhaberkorn/sciteco

commit 257a0bf128e109442dce91c4aaa1d97fed17ad1a

fixed leaking data on rubout
* Objects, that are restored with TECO_DEFINE_UNDO_OBJECT_OWN(),
  could actually leak memory on rubout since the old object was not
  deleted when overwriting it.
* Now that it is, it is crucial to at least nullify objects/pointers
  after calling the corresponding push-function.
  These conditions are now explicitly documented.
* The test suite now runs successfully under Valgrind even with full leak checking.

src/qreg.c
@@ -1708,6 +1708,8 @@ teco_machine_qregspec_auto_complete(teco_machine_qregspec_t *ctx, teco_string_t 
 void
 teco_machine_qregspec_free(teco_machine_qregspec_t *ctx)
 {
+	if (!ctx)
+		return;
 	teco_machine_stringbuilding_clear(&ctx->machine_stringbuilding);
 	teco_string_clear(&ctx->name);
 	g_free(ctx);

src/undo.h
@@ -70,15 +70,14 @@ gpointer teco_undo_push_size(teco_undo_action_t action_cb, gsize size)
  */
 /*
  * FIXME: Due to the requirements on the variable, we could be tempted to inline
- * references to it directly into the action()-function, saving the `ptr`
+ * references to it directly into the action()-function, getting rid of the `ptr`
  * in the undo token. This is however often practically not possible.
  * We could however add a variant for true global variables.
+ * Or perhaps just use TECO_DEFINE_UNDO_CALL() in these few cases.
  *
  * FIXME: Sometimes, the push-function is used only in a single compilation unit,
  * so it should be declared `static` or `static inline`.
  * Is it worth complicating our APIs in order to support that?
- *
- * FIXME: Perhaps better split this into TECO_DEFINE_UNDO_OBJECT() and TECO_DEFINE_UNDO_OBJECT_OWN()
  */
 #define __TECO_DEFINE_UNDO_OBJECT(NAME, TYPE, COPY, DELETE, DELETE_IF_DISABLED, DELETE_ON_RUN) \
 	typedef struct { \
@@ -125,24 +124,30 @@ gpointer teco_undo_push_size(teco_undo_action_t action_cb, gsize size)
  *
  * @param NAME C identifier used for name mangling.
  * @param TYPE Type of variable to restore.
- * @param COPY A global function/expression to execute in order to copy VAR.
- *             If left empty, this is an identity operation and ownership
- *             of the variable is passed to the undo token.
- * @param DELETE A global function/expression to execute in order to destruct
- *               objects of TYPE. Leave empty if destruction is not necessary.
+ * @param COPY A global function/macro to execute in order to copy VAR.
+ *   If left empty, this is an identity operation and ownership
+ *   of the variable is passed to the undo token.
+ * @param DELETE A global function/macro to execute in order to destruct
+ *   objects of TYPE. Leave empty if destruction is not necessary.
  *
  * @ingroup undo_objects
  */
 #define TECO_DEFINE_UNDO_OBJECT(NAME, TYPE, COPY, DELETE) \
 	__TECO_DEFINE_UNDO_OBJECT(NAME, TYPE, COPY, DELETE, /* don't delete if disabled */, DELETE)
+
 /**
- * @fixme _OWN variants will invalidate the variable pointer, so perhaps
- * it will be clearer to have _SET variants instead.
+ * This is like TECO_DEFINE_UNDO_OBJECT(), but passes the ownership
+ * of the variable to the undo token.
+ *
+ * If DELETE is specified, this will invalidate the variable after calling the push-function.
+ * At undo-time however, it is expected to be valid.
+ * Therefore it is crucial to set the variable afterwards or at least to nullify it.
+ * The DELETE function may consequently have to deal with NULL pointers.
  *
  * @ingroup undo_objects
  */
 #define TECO_DEFINE_UNDO_OBJECT_OWN(NAME, TYPE, DELETE) \
-	__TECO_DEFINE_UNDO_OBJECT(NAME, TYPE, /* pass ownership */, DELETE, DELETE, /* don't delete if run */)
+	__TECO_DEFINE_UNDO_OBJECT(NAME, TYPE, /* pass ownership */, DELETE, DELETE, DELETE)
 
 /** @ingroup undo_objects */
 #define TECO_DECLARE_UNDO_OBJECT(NAME, TYPE) \
@@ -150,7 +155,7 @@ gpointer teco_undo_push_size(teco_undo_action_t action_cb, gsize size)
 
 /** @ingroup undo_objects */
 #define TECO_DEFINE_UNDO_SCALAR(TYPE) \
-	TECO_DEFINE_UNDO_OBJECT_OWN(TYPE, TYPE, /* don't delete */)
+	TECO_DEFINE_UNDO_OBJECT(TYPE, TYPE, /* don't copy */, /* don't delete */)
 
 /** @ingroup undo_objects */
 #define TECO_DECLARE_UNDO_SCALAR(TYPE) \
@@ -211,13 +216,13 @@ TECO_DECLARE_UNDO_SCALAR(gconstpointer);
  * "constant" parameters.
  *
  * @param FNC Name of a global function or macro to execute.
- *            It must be a plain C identifier.
+ *   It must be a plain C identifier.
  * @param ... The parameter types of FNC (signature).
- *            Only the types without any variable names must be specified.
+ *   Only the types without any variable names must be specified.
  *
  * @fixme Sometimes, the push-function is used only in a single compilation unit,
- * so it should be declared `static` or `static inline`.
- * Is it worth complicating our APIs in order to support that?
+ *   so it should be declared `static` or `static inline`.
+ *   Is it worth complicating our APIs in order to support that?
  */
 #define TECO_DEFINE_UNDO_CALL(FNC, ...) \
 	typedef struct { \

tests/testsuite.at
@@ -287,6 +287,9 @@ AT_CHECK([$SCITECO -e '2<!foo!>'], 0, ignore, stderr)
 AT_FAIL_IF([$GREP "^Warning:" stderr])
 # Will print a warning about label redefinition, though...
 AT_CHECK([$SCITECO -e "!foo! Qa\"S^C' !foo! Qa\"S(0/0)' -Ua @O/foo/"], 0, ignore, ignore)
+# This should not leak memory or cause memory corruptions when running under
+# Valgrind or Asan:
+AT_CHECK([$SCITECO_CMDLINE "!foo!{-5D}"], 0, ignore, stderr)
 AT_CLEANUP
 
 #

src/goto-commands.c
@@ -90,6 +90,8 @@ teco_state_label_input(teco_machine_main_t *ctx, gunichar chr, GError **error)
 			teco_undo_string_own(ctx->goto_label);
 		else
 			teco_string_clear(&ctx->goto_label);
+		memset(&ctx->goto_label, 0, sizeof(ctx->goto_label));
+
 		return &teco_state_start;
 	}
 

src/qreg-commands.h
@@ -30,6 +30,7 @@ teco_state_expectqreg_reset(teco_machine_main_t *ctx)
 		teco_undo_qregspec_own(ctx->expectqreg);
 	else
 		teco_machine_qregspec_free(ctx->expectqreg);
+	ctx->expectqreg = NULL;
 }
 
 gboolean teco_state_expectqreg_initial(teco_machine_main_t *ctx, GError **error);

commit 33f71654136014bac094babaaa81d91245fdd24c

fixed rubout of Q-Register specifications
* This was a regression introduced by 257a0bf128e109442dce91c4aaa1d97fed17ad1a.
* The undo token that frees newly allocated teco_machine_qregspec_t must actually
  reset the pointer as well since any subsequent token, pushed by teco_undo_qregspec_own(),
  will expect a valid pointer.
* Could have been done via
  ctx->expectqreg = NULL;
  teco_undo_qregspec_own(ctx->expectqreg);
  but using a special clear function requires less memory and is easier to understand.
* Added test case. This wouldn't always crash, but should definitely show up in Valgrind.

src/qreg-commands.c
@@ -45,7 +45,7 @@ teco_state_expectqreg_initial(teco_machine_main_t *ctx, GError **error)
 	ctx->expectqreg = teco_machine_qregspec_new(current->expectqreg.type, ctx->qreg_table_locals,
 	                                            ctx->parent.must_undo);
 	if (ctx->parent.must_undo)
-		undo__teco_machine_qregspec_free(ctx->expectqreg);
+		undo__teco_machine_qregspec_clear(&ctx->expectqreg);
 	return TRUE;
 }
 
@@ -240,7 +240,7 @@ teco_state_queryqreg_initial(teco_machine_main_t *ctx, GError **error)
 	ctx->expectqreg = teco_machine_qregspec_new(type, ctx->qreg_table_locals,
 	                                            ctx->parent.must_undo);
 	if (ctx->parent.must_undo)
-		undo__teco_machine_qregspec_free(ctx->expectqreg);
+		undo__teco_machine_qregspec_clear(&ctx->expectqreg);
 	return TRUE;
 }
 

src/qreg.c
@@ -1715,5 +1715,12 @@ teco_machine_qregspec_free(teco_machine_qregspec_t *ctx)
 	g_free(ctx);
 }
 
-TECO_DEFINE_UNDO_CALL(teco_machine_qregspec_free, teco_machine_qregspec_t *);
+static inline void
+teco_machine_qregspec_clear(teco_machine_qregspec_t **ctx)
+{
+	teco_machine_qregspec_free(*ctx);
+	*ctx = NULL;
+}
+
+TECO_DEFINE_UNDO_CALL(teco_machine_qregspec_clear, teco_machine_qregspec_t **);
 TECO_DEFINE_UNDO_OBJECT_OWN(qregspec, teco_machine_qregspec_t *, teco_machine_qregspec_free);

src/qreg.h
@@ -252,7 +252,7 @@ void teco_machine_qregspec_free(teco_machine_qregspec_t *ctx);
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(teco_machine_qregspec_t, teco_machine_qregspec_free);
 
 /** @memberof teco_machine_qregspec_t */
-void undo__teco_machine_qregspec_free(teco_machine_qregspec_t *);
+void undo__teco_machine_qregspec_clear(teco_machine_qregspec_t **);
 TECO_DECLARE_UNDO_OBJECT(qregspec, teco_machine_qregspec_t *);
 
 #define teco_undo_qregspec_own(VAR) \

src/undo.h
@@ -143,6 +143,7 @@ gpointer teco_undo_push_size(teco_undo_action_t action_cb, gsize size)
  * At undo-time however, it is expected to be valid.
  * Therefore it is crucial to set the variable afterwards or at least to nullify it.
  * The DELETE function may consequently have to deal with NULL pointers.
+ * Also, any other undo action must always leave the variable in a valid state.
  *
  * @ingroup undo_objects
  */

tests/testsuite.at
@@ -310,6 +310,12 @@ AT_CHECK([$SCITECO_CMDLINE "@I/F/ J @I/X/ @FK/F/{-6D} Z-2\"N(0/0)'"], 0, ignore,
 AT_FAIL_IF([$GREP "^Error:" stderr])
 AT_CLEANUP
 
+AT_SETUP([Rub out Q-Register specifications])
+# This was causing memory corruptions, that would at least show up under Valgrind.
+AT_CHECK([$SCITECO_CMDLINE "GaGb{-4D}"], 0, ignore, stderr)
+AT_FAIL_IF([$GREP "^Error:" stderr])
+AT_CLEANUP
+
 AT_SETUP([Searches from macro calls])
 AT_CHECK([$SCITECO_CMDLINE "@^Um{:@S/XXX/} :Mm\"S(0/0)' Mm\"S(0/0)'"], 0, ignore, stderr)
 AT_FAIL_IF([$GREP "^Error:" stderr])